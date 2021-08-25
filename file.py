94c78a23f2e2dea91b3e72d1f88f3af7538e3d3c
Anuj B <anujbalu18@gamil.com>
2021-08-25 10:25:02 +0530

    loading users information and giving output as graph

--- file.py
+++ file.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 
 class graph:
     def __init__(self,name,age,job,salary_got,salary_spend):
@@ -7,8 +8,6 @@ class graph:
         self.job = job
         self.salary_got = salary_got
         self.salary_spend = salary_spend
-
-    #money_got = salary_got(self)
 
     def mydetail(self):
         my_detail = {
@@ -25,11 +24,11 @@ def my_file():
     age = input("Age: ")
     job = input("Job: ")
     salary_got = []
-    for i in range(2):
+    for i in range(4):
         x = int(input("Enter your salary: "))
         salary_got.append(x)
     salary_spend = []
-    for i in range(2):
+    for i in range(4):
         x = int(input("Enter your usage of Money: "))
         salary_spend.append(x)
     return {
@@ -47,22 +46,11 @@ def create_file():
     information = graph((create_file_input['name']),(create_file_input['age']),(create_file_input['job']),
                         (create_file_input['salary_got']),(create_file_input['salary_spend']))
     return information.mydetail()
-#print(create_file())
-#print(create_file())
-#h =create_file()
-#print(h)
-    #money_got = salary_got(self)
-#information = create_file()
 
-
-#print(type(create_file()))
 def save_file(info):
     information=[]
-    print(info)
     for i in info:
         information.append(i)
-
-    print(information)
     try:
         text = open('detail1.dat', 'w')
         text.write(json.dumps(information,indent=4))
@@ -70,23 +58,17 @@ def save_file(info):
     except:
         print("bye bye")
 def find_file():
-    information = open('detail.txt','r')
-    information = information.read()
-    text = list(information)
-    #print(information)
-    #x = information['name']
-    #print(x)
-    for i in text:
-        print(i)
-        #print(text[i])
-        #if i['name' == user:
-         #   print(i.salary_got)
-#find_file()
-#print(type(load_file()))
-
-
-#def find_file():
-
-
-
-#def load_file(index)
+    try:
+        user = input("Enter the user name of the account: ")
+        myfile = open('detail1.dat','r')
+        myfile= json.loads(myfile.read())
+        for text in myfile:
+            if text['name'] == user:
+                x = text['salary_got']
+                y = text['salary_spend']
+                fig, axes = plt.subplots(figsize=(12, 3), dpi=100)
+                axes.plot(x,'g')
+                axes.plot(y, 'r.-')
+                plt.show()
+    except:
+        print("something went wrong....")
--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import matplotlib.pyplot as plt
 import json
-#import user_input
 import file
-import os
 
-#money_got = user_input.graph.salary_got('self')
-#print(money_got)
-step = 0
 info = []
-text = open('detail1.dat','r')
-text_loaded = json.loads(text.read())
-for text in text_loaded:
-    info.append(text)
-print(info)
-while step != 'x':
-    step = int(input("Enter a number: "))
-    if step == 1:
-        info.append(file.create_file())
-        print(info)
-    elif step == 2:
-        file.save_file(info)
-        #print(type(save))
-    elif step == 3:
-        print(file.find_file(info,name ='anuj'))
-plt.plot(money_got)
+try:
+    text = open('detail1.dat','r')
+    text_loaded = json.loads(text.read())
+    for text in text_loaded:
+        info.append(text)
+except:
+    print("file is empty....")
+    print("please load your information")
+while True:
+    try:
+        step = int(input("Enter a number: "))
+        if step == 1:
+            info.append(file.create_file())
+        elif step == 2:
+            file.save_file(info)
 
+        elif step == 3:
+            file.find_file()
+    except:
+        print("Check the number you entered")
 
-plt.show()
